3c10b6791e85ae974bab2fd9f81f9bafa0c2fa80
Kevin Alama <alama1992@gmail.com>
2020-12-29 22:30:29 -0500

    added basic functionality with output log messages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import Navigation from './components/Navigation/Navigation'
-import Logo from './components/Logo/Logo'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
 
-    </div>
-  );
+  onInputChange = (event) => {
+    console.info(event.target.value);
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <Particles className='particles'/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange}/>  
+      </div>
+    );
+  }  
 }
 
 export default App;
